e587cba0f9dad7e8d35348e354f326f20857263d
Prabhakar Naik K <prabhakarnaik225@gmail.com>
2024-06-02 10:43:30 +0530

    project setup

---
+++ src/main/java/com/springboot/SpringBootCrudSqlApplication.java
@@ -0,0 +1,14 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudSqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCrudSqlApplication.class, args);
+        System.out.println("Application Running Success...@");
+    }
+
+}
